f6d519aee19920684d6604a4fc9e19e4407595aa
IBruteDude <kamarf505@gmail.com>
2023-03-16 03:48:17 -0700

    printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point to project
+ * Return: returns exit status
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
