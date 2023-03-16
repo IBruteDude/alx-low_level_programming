b0f653a01dbb4c3569c2f7dfce9b0489b940ad7b
IBruteDude <kamarf505@gmail.com>
2023-03-16 03:11:19 -0700

    printf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point to project
+ * Return: returns exit status
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\nSize of a float: %d byte(s)",
+		sizeof(char),
+		sizeof(long long int),
+		sizeof(float));
+	return (0);
+}
