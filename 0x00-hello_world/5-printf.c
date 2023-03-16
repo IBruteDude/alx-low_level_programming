1a5ea2bf364bc6666ed34d46f164091a0681cd93
IBruteDude <kamarf505@gmail.com>
2023-03-16 02:44:44 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+/**
+ * main - Entry to main to print string
+ * Return: returns exit status
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
