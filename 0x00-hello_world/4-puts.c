be27102b7f61258af2e299f8772d26eff5797485
IBruteDude <kamarf505@gmail.com>
2023-03-16 02:35:24 -0700

    assembler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include "stdio.h"
 
+/**
+ * main - Entry to main to print string
+ * Return: returns exit status
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
