255ce96a5986bd8002923739e500a1144ccc381d
IBruteDude <kamarf505@gmail.com>
2023-03-17 00:30:43 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,18 +8,15 @@
 
 int main(void)
 {
-	for (int i = '0';; i++)
+	for (int i = '0'; i < '9'; i++)
 	{
 		putchar(i);
-		if (i <= '9')
+		if (i != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			break;
-		}
 	}
+	putchar('\n');
 	return (0);
 }
