367e1a44baa5dc8b1cfde4adf5e31b56823cb3fa
IBruteDude <kamarf505@gmail.com>
2023-03-16 23:51:42 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point to project
+ * Return: returns exit status
+ */
+int main(void)
+{
+	/* like a fucking chad */
+	for (char c = 'a'; c <= 'z'; putchar(c++))
+		if(c == 'e' || c == 'q') c++;
+	putchar('\n');
+	return (0);
+}
