6f25792c66c39ef927a97d4cc951fb35b877f50b
IBruteDude <kamarf505@gmail.com>
2023-03-17 00:05:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point to project
+ * Return: returns exit status
+ */
+int main(void)
+{
+	for (char i = '0'; i <= '9'; putchar(i++))
+		;
+	putchar('\n');
+	return (0);
+}
