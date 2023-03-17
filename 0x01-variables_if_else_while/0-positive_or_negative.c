10d2bb481af355967b53649fd76358f9227dc206
IBruteDude <kamarf505@gmail.com>
2023-03-16 22:29:26 -0700

    p/n/0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/**
+ * main - Entry point to code
+ * Description: generate a random number then check for sign
+ * Return: exit status
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n == 0)
+		puts("0 is zero\n");
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is negative\n", n);
+	return (0);
+}
+
