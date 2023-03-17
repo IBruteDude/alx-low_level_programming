7ec6e0c6c4998726680d9c3e717ca4f6711299bf
IBruteDude <kamarf505@gmail.com>
2023-03-16 23:02:48 -0700

    pa1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point to project
+ * Return: returns exit status
+ */
+int main(void)
+{
+	/* like a fucking chad */
+	for (char c = 'a'; c <= 'z'; putchar(c++))
+		;
+	putchar('\n');
+	return (0);
+}
