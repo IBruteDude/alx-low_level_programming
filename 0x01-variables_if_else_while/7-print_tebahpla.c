7164d82910cba94ac426a7dda5dfd6523a2e5202
IBruteDude <kamarf505@gmail.com>
2023-03-17 00:10:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point to project
+ * Return: returns exit status
+ */
+int main(void)
+{
+	/* like a fucking chad */
+	for (char c = 'z'; c <= 'a'; putchar(c--))
+		;
+	putchar('\n');
+	return (0);
+}
