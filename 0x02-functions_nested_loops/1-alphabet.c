feff074b23ff3f736c6b965adf7f8fb0ba0a736d
IBruteDude <kamarf505@gmail.com>
2023-03-21 11:44:16 -0700

    alphabit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabit in small case
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	for (; c <= 'z'; _putchar(c++))
+		;
+	_putchar('\n');
+}
