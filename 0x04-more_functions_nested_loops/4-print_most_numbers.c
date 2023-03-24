febeecaad818a75fdb6405d4bbf91c546a29dae8
IBruteDude <kamarf505@gmail.com>
2023-03-24 01:13:03 -0700

    most digs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,13 @@
-void print_most_numbers(void);
+#include "main.h"
+/**
+ * print_most_numbers - prints the ten digits except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; _putchar(c++))
+		if (c == '2' || c == '4')
+			continue;
+	_putchar('\n');
+}
