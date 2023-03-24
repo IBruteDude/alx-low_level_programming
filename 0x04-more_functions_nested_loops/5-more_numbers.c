44d442b9f5da8154752f7af4beaed2f23dea3d6b
IBruteDude <kamarf505@gmail.com>
2023-03-24 02:06:21 -0700

    more digs

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,20 @@
-void more_numbers(void);
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		int i;
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+				_putchar('1');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
