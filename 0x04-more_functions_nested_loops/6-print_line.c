9ccb6f16fccc7ede519348a55e3f42f5d53e4c42
IBruteDude <kamarf505@gmail.com>
2023-03-24 02:12:48 -0700

    more digs

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,11 @@
-void print_line(int n);
+#include "main.h"
+/**
+ * print_line - prints a line of n length
+ * @n: number of underscores
+ */
+void print_line(int n)
+{
+	for (; n > 0; n--)
+		_putchar('_');
+	_putchar('\n');
+}
