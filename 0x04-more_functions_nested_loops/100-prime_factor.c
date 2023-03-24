35d4da9c72abbe47e386ebda9782052badd62fbc
IBruteDude <kamarf505@gmail.com>
2023-03-24 07:33:05 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,17 @@
-#include ""
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 for success
+ */
+int main(void)
+{
+	long int num = 612852475143, comp = 3;
+	while (num != 1)
+	{
+		while (!(num % comp))
+			num /= comp;
+		comp += 2;
+	}
+	printf("%lu\n", comp);
+	return (0);
+}
