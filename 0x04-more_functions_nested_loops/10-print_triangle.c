133eb08cf40b245aa94e210ec486c650c4054303
IBruteDude <kamarf505@gmail.com>
2023-03-24 05:56:17 -0700

    tri

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,25 @@
-void print_triangle(int size);
+#include "main.h"
+/**
+ * print_triangle - prints a triangle of hashtags
+ * @size: triangle base length
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(j < (size - i - 1) ? ' ' : '#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
