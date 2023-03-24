bea676042eaac28f425c70d38e4768548e663e30
IBruteDude <kamarf505@gmail.com>
2023-03-24 03:58:28 -0700

    integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,28 @@
-void print_number(int n);
+#include "main.h"
+/**
+ * print_number - prints a formatted integer value
+ * @n: input integer
+ */
+void print_number(int n)
+{
+	int i, nth_digit, no = 1, power = 10;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n >= power)
+	{
+		no++;
+		power *= 10;
+	}
+
+	for (i = 0; i < no; i++)
+	{
+		power /= 10;
+		nth_digit = n / power;
+		_putchar(nth_digit % 10 + '0');
+	}
+}
