9c645201f7b3b6d49bc53e4cc2460c30be8f719d
IBruteDude <kamarf505@gmail.com>
2023-03-24 07:41:59 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,11 +6,13 @@
 int main(void)
 {
 	long int num = 612852475143, comp = 3;
+
 	while (1)
 	{
 		while (!(num % comp))
 			num /= comp;
-		if (num == 1) break;
+		if (num == 1)
+			break;
 		comp += 2;
 	}
 	printf("%lu\n", comp);
