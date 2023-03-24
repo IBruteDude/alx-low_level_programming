0ef907d62f1c4a1a88234994a3384835f2dd717d
IBruteDude <kamarf505@gmail.com>
2023-03-24 02:08:46 -0700

    more digs

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,10 +9,11 @@ void more_numbers(void)
 	for (j = 0; j < 10; j++)
 	{
 		int i;
+
 		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
-				_putchar('1');
+				_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
