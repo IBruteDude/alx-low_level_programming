c361cd8aebf8ca9ee809ca9d0f20b66dcafe78f2
IBruteDude <kamarf505@gmail.com>
2023-03-24 00:18:09 -0700

    digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,12 @@
-void print_numbers(void);
+#include "main.h"
+/**
+ * print_numbers - prints the ten digits
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; _putchar(c++))
+		;
+	_putchar('\n');
+}
