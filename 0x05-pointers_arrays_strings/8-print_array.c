0c81f526980b083613c46e7787e825592d0eeade
IBruteDude <kamarf505@gmail.com>
2023-03-29 00:34:19 -0700

    strcpy

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,9 +6,11 @@
  */
 void print_array(int *a, int n)
 {
-	int i = 1;
-
-	printf("%i", a[0]);
-	for (; i < n; i++)
-		printf(", %i", a[i]);
+	int i;
+	
+	if (n >= 0)
+		printf("%i", a[0]);
+		for (i = 1; i < n; i++)
+			printf(", %i", a[i]);
+		putchar('\n');
 }
