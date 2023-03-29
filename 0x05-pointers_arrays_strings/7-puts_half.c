60a4d73e73abb61ab8d6a9cff21f55b383da466e
IBruteDude <kamarf505@gmail.com>
2023-03-29 00:04:25 -0700

    puttings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,15 @@
-void puts_half(char *str);
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - put second half of  string in stdout
+ * @str: string NTCS
+ */
+void puts_half(char *str)
+{
+	int s = strlen(str);
+	
+	str = str + s - s / 2;
+	while (*str != 0)
+		_putchar(*(str++));
+	_putchar('\n');
+}
