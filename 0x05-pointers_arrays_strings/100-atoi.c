5355b4a5220b1f9e08fb5aa5e942deb7082c75e7
IBruteDude <kamarf505@gmail.com>
2023-03-28 18:39:17 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,33 @@
-int _atoi(char *s);
+#include "main.h"
+#define dig(c) ('0' <= c && c <= '9')
+/**
+ * _atoi - extract an int from a string
+ * @s: input NTCS
+ * Return: found int
+ */
+int _atoi(char *s)
+{
+	int neg = 0;
+	char *p;
+	int n = 0/*, fl = _strlen(s)*/, dl = 0, pow = 1;
+
+	while (!dig(*s))
+		s++;
+	if (*(s - 1) == '-')
+		neg = 1;
+	while (dig(*s))
+	{
+		dl++;
+		pow *= 10;
+		s++;
+	}
+	p = s;
+	s -= dl;
+	while (s != p)
+	{
+		pow /= 10;
+		n += pow * (*s - '0');
+		s++;
+	}
+	return ((neg) ? -n : n);
+}
