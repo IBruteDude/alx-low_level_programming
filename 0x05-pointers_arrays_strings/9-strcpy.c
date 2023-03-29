324b6860e2f4af0b8944214b9a64b9b6e35f9f5a
IBruteDude <kamarf505@gmail.com>
2023-03-29 00:28:49 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,14 @@
-char *_strcpy(char *dest, char *src);
+/**
+ * _strcpy - copys a string
+ * @dest: destination address
+ * @src: soure address
+ * Return: copied destination address
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *store = dest;
+	while (*src != 0)
+		*(dest++) = *(src++);
+	*dest = 0;
+	return (store);
+}
