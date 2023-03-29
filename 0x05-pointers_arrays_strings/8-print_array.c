14d74a0b377065514e34f98288444ac3bd892506
IBruteDude <kamarf505@gmail.com>
2023-03-29 00:11:22 -0700

    arr no point

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,14 @@
-void print_array(int *a, int n);
+#include <stdio.h>
+/**
+ * print_array - prints the elements of an array
+ * @a: input array
+ * @n: number of array elements
+ */
+void print_array(int *a, int n)
+{
+	int i = 1;
+
+	printf("%i", a[0]);
+	for (; i < n; i++)
+		printf(", %i", a[i]);
+}
