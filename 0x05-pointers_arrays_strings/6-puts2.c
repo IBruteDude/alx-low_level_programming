76030cdfed3e1505b14bb4fb8e265eeb6f44b67f
IBruteDude <kamarf505@gmail.com>
2023-03-28 23:51:37 -0700

    reversal

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,15 @@
-void puts2(char *str);
+#include "main.h"
+/**
+ * _puts2 - put stripped string in stdout
+ * @str: string NTCS
+ */
+void _puts2(char *str)
+{
+	/* isn't memory unsafe - won't check one slot out of bound if the and short-circuits first checking */
+	while (*str != 0 && *(str + 1) != 0)
+	{
+		_putchar(*(str));
+		str += 2;
+	}
+	_putchar('\n');
+}
