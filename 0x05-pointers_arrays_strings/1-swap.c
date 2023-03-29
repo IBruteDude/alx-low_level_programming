709186bb5fae01b3e5f7cfaefa5053878bde23c0
IBruteDude <kamarf505@gmail.com>
2023-03-29 00:54:27 -0700

    strcpy

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,23 +6,7 @@
  */
 void swap_int(int *a, int *b)
 {
-	bool a_sign = (*a < 0), b_sign = (*b < 0);
-
-	if (*a >= *b)
-	{
-		*a -= *b;
-		*b += *a;
-		*a -= *b;
-	}
-	else
-	{
-		*b -= *a;
-		*a += *b;
-		*b -= *a;
-	}
-
-	if (a_sign && (*b >= 0))
-		*b = -(*b);
-	if (b_sign && (*a >= 0))
-		*a = -(*a);
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
