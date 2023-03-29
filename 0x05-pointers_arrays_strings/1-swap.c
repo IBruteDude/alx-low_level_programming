0ff797aaeb597adb22693f921d9d5e417aeab9ff
IBruteDude <kamarf505@gmail.com>
2023-03-28 21:25:16 -0700

    atoi

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 /*
  * swap_int - swaps int
  * @a: 1st int
@@ -5,7 +6,23 @@
  */
 void swap_int(int *a, int *b)
 {
-	*a += *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	bool a_sign = (*a < 0), b_sign = (*b < 0);
+
+	if (*a >= *b)
+	{
+		*a -= *b;
+		*b += *a;
+		*a -= *b;
+	}
+	else
+	{
+		*b -= *a;
+		*a += *b;
+		*b -= *a;
+	}
+
+	if (a_sign && (*b >= 0))
+		*b = -(*b);
+	if (b_sign && (*a >= 0))
+		*a = -(*a);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,11 @@
-void _puts(char *str);
+#include <stdio.h>
+/**
+ * _puts - put string in stdout
+ * @str: string NTCS
+ */
+void _puts(char *str)
+{
+	while (*str != 0)
+		putchar(*(str++));
+	putchar('\n');
+}
