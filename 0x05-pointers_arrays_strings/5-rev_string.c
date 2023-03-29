8202ead75013c76f1a222a5ab76523720d2ad3ce
IBruteDude <kamarf505@gmail.com>
2023-03-28 22:21:14 -0700

    reversal

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,19 @@
 #include <string.h>
-
+/**
+ * swap - swaps two chars
+ * @a: 1st char
+ * @b: 2nd char
+ */
 void swap(char *a, char *b)
 {
 	char temp = *a;
 	*a = *b;
 	*b = temp;
 }
+/**
+ * rev_string - reverses a string
+ * @s: input string
+ */
 void rev_string(char *s)
 {
 	int i, size = strlen(s);
