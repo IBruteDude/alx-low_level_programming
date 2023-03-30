564372dceb8a752e7adc26c0a3c9108b4291b52b
IBruteDude <kamarf505@gmail.com>
2023-03-30 10:39:19 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,9 @@
-char *_strcat(char *dest, char *src);
+#include <string.h>
+char *_strcat(char *dest, char *src)
+{
+	int i, dsize = strlen(dest), ssize = strlen(src);
+
+	for (i = 0; i <= ssize; ++i)
+		dest[dsize + i] = src[i];
+	return dest;
+}
