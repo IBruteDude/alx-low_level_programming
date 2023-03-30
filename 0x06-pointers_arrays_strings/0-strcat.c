8d8dd1ada15ab74243c9cce26e8d25889c327383
IBruteDude <kamarf505@gmail.com>
2023-03-30 10:43:02 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,15 @@
 #include <string.h>
+/**
+ * _strcat - string catenation
+ * @dest: destination
+ * @src: source
+ * Return: destination pointer
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i, dsize = strlen(dest), ssize = strlen(src);
 
 	for (i = 0; i <= ssize; ++i)
 		dest[dsize + i] = src[i];
-	return dest;
+	return (dest);
 }
