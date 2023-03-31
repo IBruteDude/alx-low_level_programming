33af0829898d067a235b639277baabd5362caf1e
IBruteDude <kamarf505@gmail.com>
2023-03-30 19:57:29 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,27 @@
-char *leet(char *);
+char *leet(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		switch(s[i])
+		{
+		case 'a': case 'A':
+			s[i] = '4';
+			break;
+		case 'e': case 'E':
+			s[i] = '3';
+			break;
+		case 'o': case 'O':
+			s[i] = '0';
+			break;
+		case 't': case 'T':
+			s[i] = '7';
+			break;
+		case 'l': case 'L':
+			s[i] = '1';
+			break;
+		}
+	}
+	return (s);
+}
