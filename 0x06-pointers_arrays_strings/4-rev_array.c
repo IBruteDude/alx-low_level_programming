ab4c881a35b284e1925e45b23f5ced1873c7db02
IBruteDude <kamarf505@gmail.com>
2023-03-30 19:21:37 -0700

    revarray

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,25 @@
-void reverse_array(int *a, int n);
+#include <stdbool.h>
+/**
+ * swap_int - swaps int
+ * @a: 1st int
+ * @b: 2nd int
+ */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * reverse_array - reverses array
+ * @a: int array
+ * @n: array length
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n + 1) / 2; ++i)
+		swap(a + i, a + (n - 1) -i);
+}
