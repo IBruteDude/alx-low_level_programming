ba2b97584d8c8762f801c1b12af68039946557a7
IBruteDude <kamarf505@gmail.com>
2023-03-30 18:55:15 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,16 @@
-char *_strncat(char *dest, char *src, int n);
+#include <string.h>
+/**
+ * _strcat - string catenation
+ * @dest: destination
+ * @src: source
+ * @n: limit
+ * Return: destination pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, dsize = strlen(dest), ssize = strlen(src);
+
+	for (i = 0; i <= ssize && i < n; ++i)
+		dest[dsize + i] = src[i];
+	return (dest);
+}
