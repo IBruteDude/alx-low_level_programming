017785a318fdaa2fc60aa1db5dca1c320d8cdd29
IBruteDude <kamarf505@gmail.com>
2023-03-30 19:04:40 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,21 @@
-char *_strncpy(char *dest, char *src, int n);
+#include <string.h>
+/**
+ * _strncpy - string catenation
+ * @dest: destination
+ * @src: source
+ * @n: limit
+ * Description: concatenate atmost n from source
+ * Return: destination pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int src_end = 0;
+	int i;
+
+	for (i = 0; i <= ssize && i < n; ++i)
+		if (dest[i] != '\0' && src[i] != '\0')
+			dest[i] = src[i];
+		else
+			break
+	return (dest);
+}
