653fca24ffeee7fb40eca00fe4971752dff38839
IBruteDude <kamarf505@gmail.com>
2023-04-03 15:21:35 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,8 @@
-void print_chessboard(char (*a)[8]);
+#include <stdio.h>
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	for (i = 0; i < 8; i++)
+		for (j = 0; j <= 8; j++)
+			putchar((j != 8) ? (a[i][j]) : '\n');
+}
