4bc777ca2e055345af178e790368316a6b0dbdc2
IBruteDude <kamarf505@gmail.com>
2023-04-03 15:07:44 -0700

    diag

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,12 @@
-void print_diagsums(int *a, int size);
+#include <stdio.h>
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0, sum2 = 0, size_c = size;
+
+	while (size_c--)
+		sum1 += *(a + (size_c * size) + size - 1 - size_c);
+	size_c = size;
+	while (size_c--)
+		sum2 += *(a + (size_c * size) + size_c);
+	printf("%i, %i\n", sum2, sum1);
+}
