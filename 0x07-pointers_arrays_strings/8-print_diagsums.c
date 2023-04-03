76e53ae32c4466771c4af840a9ee779d92122f0c
IBruteDude <kamarf505@gmail.com>
2023-04-03 15:09:58 -0700

    diag

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * print_diagsums - prints the diagonal of an array of arrays matrix
+ * @a: address of the first element in the matrix
+ * @size: size of each row/column in the matrix
+ */
 void print_diagsums(int *a, int size)
 {
 	int sum1 = 0, sum2 = 0, size_c = size;
