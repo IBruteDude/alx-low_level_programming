fee5883fdc186ab667cef9a4682490fe0316a735
IBruteDude <kamarf505@gmail.com>
2023-04-03 14:25:57 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _memset - set first n bytes of n to b
+ * @s: input string
+ * @b: char to be filled with
+ * @n: number of bytes to be filled
+ * Return: output string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+		s[n] = b;
+	return (dest);
+}
+
