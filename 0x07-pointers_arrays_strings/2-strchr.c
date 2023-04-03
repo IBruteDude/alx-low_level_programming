7c48cf9dd488f53fdb24cd6446af896a50e088e7
IBruteDude <kamarf505@gmail.com>
2023-04-03 14:42:04 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,14 @@
-char *_strchr(char *s, char c);
+#include "main.h"
+/**
+ * _strchr - search for c in s
+ * @s: input string
+ * @c: character query
+ * Return: output string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s == '\0')
+		if (*(s++) == c)
+			return (s - 1);
+	return (void *) 0;
+}
