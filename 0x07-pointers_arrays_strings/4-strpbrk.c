22a49e76c6a542a87d12db449ee911178bad1c4b
IBruteDude <kamarf505@gmail.com>
2023-04-03 19:57:42 -0700

    str pbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,22 @@
-char *_strpbrk(char *s, char *accept);
+/**
+ * _strpbrk - find the first of s agreeing with accept
+ * @s: input string
+ * @accept: tested characters
+ * Return: number of the first successful test
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int j;
+
+	while (*(s++) != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+			if (*(s - 1) == accept[j])
+			{
+				break;
+			}
+		if (accept[j] != '\0')
+			break;
+	}
+	return (s - 1);
+}
