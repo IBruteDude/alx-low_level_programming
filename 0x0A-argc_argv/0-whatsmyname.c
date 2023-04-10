1d516a8841990f51aaa25525a623dbbdf9f28ccc
IBruteDude <kamarf505@gmail.com>
2023-04-10 16:47:06 -0700

    Initial Setup

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - gets arguments to print the name of the program
+ * @argc: argument count
+ * @argv: argument values
+ * Return: 0 successful exit status
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
