f2e1f434107142d16f39d68536c6dc3ca6180f22
IBruteDude <kamarf505@gmail.com>
2023-04-11 12:04:44 -0700

    string dups

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,22 @@
-char *_strdup(char *str);
+#include <stdlib.h>
+/**
+ * _strdup - duplicates a string
+ * @str: input string
+ * Return: duplicate string 
+ */
+char *_strdup(char *str)
+{
+	if (str != NULL)
+	{
+		int s = sizeof(str);
+		char *dup = malloc(s);
+
+		while (s--)
+			dup[s] = str[s];
+		return (dup);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
