0619720f4680cf1c8bacf0c6900849ac536b3469
IBruteDude <kamarf505@gmail.com>
2023-04-10 19:31:24 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,21 +10,13 @@
 
 int main(int argc, char *argv[])
 {
-	int i, s, n = atoi(argv[1]), change = 0, curr[] = {25, 10, 5, 2, 1};
+	int i, n = atoi(argv[1]), change = 0, curr[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	s = strlen(argv[1]);
-	for (i = 0; i < s; ++i)
-		if (!(argv[1][0] == '-') && (argv[1][i] < '0' || '9' < argv[1][i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-
 
 	if (n <= 0)
 	{
