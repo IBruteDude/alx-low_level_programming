400ecaac7a632dcf3a9052d3b60f7e315f32a616
IBruteDude <kamarf505@gmail.com>
2023-04-11 12:35:06 -0700

    string dups

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,25 @@
-char *str_concat(char *s1, char *s2);
+#include <string.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: seconde string
+ * Return: concatenated string if successful, NULL if failed
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int s = strlen(s1);
+	char *cat = malloc(s + strlen(s2) + 1);
+
+	if (s1 != NULL && s2 == NULL)
+		return (s1);
+	if (s2 != NULL && s1 == NULL)
+		return (s2);
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	if (cat == NULL)
+		return (NULL);
+	strcpy(cat, s1);
+	strcpy(cat + s, s2);
+	return (cat);
+}
