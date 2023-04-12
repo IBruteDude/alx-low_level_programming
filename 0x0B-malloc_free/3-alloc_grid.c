da91ae3fc3c3aa874ded7331ea17eaa8f81b22f5
IBruteDude <kamarf505@gmail.com>
2023-04-11 22:00:01 -0700

    ALLOC

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 /**
  * alloc_grid - allocate a grid of ints
  * @width: width
@@ -7,14 +8,40 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, **matrix;
+	int si = sizeof(int), sip = sizeof(int *);
+	int i, j, **matrix, **temp;
 
 	if (height == 0 || width == 0)
 		return (NULL);
 
-	matrix = malloc(sizeof(int *) * height);
-	if (matrix == NULL)
+	/* allocate space for a block of pointers to adjacent int blocks */
+	matrix = malloc((sip * height) + (si * height * width));
+	temp = malloc(sip * height);
+	if (matrix == NULL || temp == NULL)
 		return (NULL);
+	for (i = 0; i < height; ++i)
+	{
+		temp[i] = matrix[i] = malloc(si * width);
+		memcpy(matrix[i], (int *) 	/* cast back to a pointer */
+		/* manipulate address of matrix with integer arithmetic */
+			       	(((long) &matrix)
+		/* pointer array offset */
+				+ (height * sip)
+		/* integer arrays offset */
+				+ (i * width * si)) 
+				, sip);
+	}
+	/* initialize all with zeros */
+	si = 0;
+	for (i = 0; i < height; ++i)
+	{
+		for (j = 0; j < width; ++j)
+			matrix[i][j] = si++;
+		free(temp[i]);
+	}
+	free(temp);
+
+	/**
 	for (i = 0; i < height; ++i)
 	{
 		matrix[i] = malloc(sizeof(int) *  width);
@@ -33,7 +60,6 @@ int **alloc_grid(int width, int height)
 				matrix[i][j] = 0;
 		}
 	}
-	for (i = 0; i < height; ++i)
-		free(matrix[i]);
+	*/
 	return (matrix);
 }
