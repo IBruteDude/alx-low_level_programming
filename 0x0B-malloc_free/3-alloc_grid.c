bf14bb09add2a8edc81be5fa0031ec8d95b461d5
IBruteDude <kamarf505@gmail.com>
2023-04-11 19:43:39 -0700

    ALLOC

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,15 +7,26 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, **matrix = malloc(sizeof(int) * height);
-	
+	int i, j, **matrix;
+	if (height == 0 || width == 0)
+		return NULL;
+
+	matrix = malloc(sizeof(int) * height);
 	if (matrix == NULL)
 		return NULL;
 	for (i = 0; i < height; ++i)
 	{
 		matrix[i] = malloc(sizeof(int) *  width);
 		if (matrix[i] == NULL)
+		{
+			for (; i > 0; --i)
+				free(matrix[i - 1]);
+			free(matrix);
 			return NULL;
+		}
+		else
+			for (j = 0; j < width; ++j)
+			       matrix[i][j] = 0;	
 	}
 	return matrix;
 }
