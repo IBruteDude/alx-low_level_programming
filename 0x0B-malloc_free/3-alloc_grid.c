c336aca5ed45f5e4b29f30491277ba96fd0b27b2
IBruteDude <kamarf505@gmail.com>
2023-04-11 19:18:21 -0700

    ALLOC

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,1 +1,21 @@
-int **alloc_grid(int width, int height);
+#include <stdlib.h>
+/**
+ * alloc_grid - allocate a grid of ints
+ * @width: width
+ * @height: height
+ * Return: address of grid, or NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, **matrix = malloc(sizeof(int) * height);
+	
+	if (matrix == NULL)
+		return NULL;
+	for (i = 0; i < height; ++i)
+	{
+		matrix[i] = malloc(sizeof(int) *  width);
+		if (matrix[i] == NULL)
+			return NULL;
+	}
+	return matrix;
+}
