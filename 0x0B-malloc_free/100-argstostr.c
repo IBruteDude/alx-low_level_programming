eacd6cbf845b99354344f158ae40128b2d44327f
IBruteDude <kamarf505@gmail.com>
2023-04-12 07:26:23 -0700

    advanced string stuff alhamdullilah done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,1 +1,29 @@
-char *argstostr(int ac, char **av);
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * argstostr - concatenates two strings
+ * @ac: number of arguments
+ * @av: arguments array
+ * Return: concatenated string if successful, NULL if failed
+ */
+char *argstostr(int ac, char **av)
+{
+	char *cat;
+	int i, total_length = 0;
+
+	for (i = 0; i < ac; ++i)
+		total_length += strlen(av[i]) + 1;
+	cat = malloc(total_length + 1);
+	if (cat == NULL || av == NULL || ac <= 0)
+		return (NULL);
+	total_length = 0;
+	for (i = 0; i < ac; ++i)
+	{
+		strcpy(cat + total_length, av[i]);
+		total_length += strlen(av[i]) + 1;
+		cat[total_length - 1] = '\n';
+	}
+	cat[total_length] = '\0';
+	return (cat);
+}
--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,1 +1,38 @@
-char **strtow(char *str);
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * strtow - splits a string to words
+ * @str: input string
+ * Return: word string array; or NULL if failed
+ */
+char **strtow(char *str)
+{
+	int i, n = 0;
+	char *s = str, *word, **words = calloc(sizeof(char *) * (strlen(str) / 2), sizeof(char *));
+	
+	if (words == NULL)
+		return (NULL);
+	if (str == NULL)
+	{
+		free(words);
+		return (NULL);
+	}
+	while (*(s))
+	{
+		i = 0;
+		while(isspace(*s))
+			s++;
+		while(!isspace(*(s++)))
+			i++;
+		s -= i + 1;
+		word = malloc(i + 1);
+		strncpy(word, s, i);
+		s += i + 1;
+		if (*(s))
+			words[n] = word;
+		n++;
+	}
+	words = realloc(words, sizeof(char *) * n);
+	return (words);
+}
