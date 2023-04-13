50348bff981b434ca796ffc876b27763450f40a8
IBruteDude <kamarf505@gmail.com>
2023-04-12 21:34:58 -0700

    mallocshyted

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,13 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - malloc but exits with code 98 if failed
+ * @b: size of memory
+ * Return: pointer to allocated memory
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
-	if (p == (void *)0)
+	if (p == NULL)
 		exit(98);
 	return (p);
 }
