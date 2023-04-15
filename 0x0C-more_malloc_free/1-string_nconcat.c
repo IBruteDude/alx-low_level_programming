b8cbc45711646a2e0e9f1270e850fbbbc2c62db0
IBruteDude <kamarf505@gmail.com>
2023-04-14 23:04:50 -0700

    mult algorithm

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,29 +9,18 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char s1N = 0, s2N = 0, *str;
+	char *str;
+	unsigned int min = (strlen(s2) < n) ? strlen(s2) : n;
 
 	if (s1 == NULL)
-	{
-		s1 = malloc(1);
-		*s1 = '\0';
-		s1N = 1;
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2 = malloc(1);
-		*s2 = '\0';
-		s2N = 1;
-	}
-	str = malloc(sizeof(s1) + ((sizeof(s2) - 1 < n) ? sizeof(s2) - 1 : n));
+		s2 = "";
+	str = malloc(strlen(s1) + min + 1);
 	if (str == NULL)
 		return (NULL);
 	strcpy(str, s1);
 	strncpy(str + strlen(s1), s2, n);
-	str[sizeof(s1) + ((strlen(s2) < n) ? strlen(s2) : n) - 1] = '\0';
-	if (s1N)
-		free(s1);
-	if (s2N)
-		free(s2);
+	str[strlen(s1) + min] = '\0';
 	return (str);
 }
