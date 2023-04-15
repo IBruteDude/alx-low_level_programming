01a4b39e43e4a0a7fe79055b248481ac22f71cea
IBruteDude <kamarf505@gmail.com>
2023-04-15 00:47:29 -0700

    shokran

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,9 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
 
 /**
  * multiply - precise implementation of the multiplication algorithm
@@ -11,30 +13,25 @@
  */
 void multiply(char *n1, char *n2, char *res)
 {
-	int *product, s1 = strlen(n1), s2 = strlen(n2), a_i, b_i, carry;
-
-	product = calloc(s1 + s2 + 1, sizeof(int));
-	if (product == NULL)
-		printf("Error\n"), exit(98);
+	int idx, s1 = strlen(n1), s2 = strlen(n2), a_i, b_i, carry;
 
 	for (b_i = 1; b_i <= s2; b_i++)
 	{
 		carry = 0;
 		for (a_i = 1; a_i <= s1; a_i++)
 		{
-			product[a_i + b_i - 1] += carry +
-			       	((n1[s1 - a_i] - '0') * (n2[s2 - b_i] - '0'));
-			carry = product[a_i + b_i - 1] / 10;
-			product[a_i + b_i - 1] %= 10;
-		}
-		product[b_i + s1] = carry;
-	}
+			idx = s1 + s2 - (a_i + b_i - 1);
+			res[idx] -= '0';
 
-	for (a_i = 0; a_i < s1 + s2; a_i++)
-		res[a_i] = product[s1 + s2 - a_i] + '0';
-	free(product);
-	if (res[0] == '0')
-		memcpy(res, res + 1, s1 + s2);
+			res[idx] += carry +
+				((n1[s1 - a_i] - '0') * (n2[s2 - b_i] - '0'));
+			carry = res[idx] / 10;
+			res[idx] %= 10;
+
+			res[idx] += '0';
+		}
+		res[s2 - b_i] += carry;
+	}
 }
 
 /**
@@ -43,7 +40,7 @@ void multiply(char *n1, char *n2, char *res)
  * @argv: arguments array
  * Return: exit status
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	char failed = 0, *res, *n1, *n2;
 	int i, s1, s2;
@@ -64,7 +61,7 @@ int main(int argc, char *argv[])
 		if (!isdigit(n2[i]))
 			failed = 1;
 	s1 = strlen(n1), s2 = strlen(n2);
-	res = malloc(s1 + s2 + 1);
+	res = malloc(s1 + s2);
 	if (res == NULL)
 		failed = 1;
 	if (failed)
@@ -78,11 +75,13 @@ int main(int argc, char *argv[])
 	/* All safe */
 	for (i = 0; i < s1 + s2; i++)
 		res[i] = '0';
-	res[s1 + s2] = '\0';
 
 	/* All ready */
 	multiply(n1, n2, res);
-	printf("%s\n", res);
+	i = s1 + s2;
+	while (i--)
+		_putchar(res[s1 + s2 - 1 - i]);
+	_putchar('\n');
 	free(res);
 	return (0);
 }
