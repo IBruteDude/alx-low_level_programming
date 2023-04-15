e0021386ea4f2945d0e1800c618406c211c82818
IBruteDude <kamarf505@gmail.com>
2023-04-14 22:45:20 -0700

    mult algorithm

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,10 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+
+
+void multiply(char *n1, char *n2, char *res)
+{
+	int idx, s1 = strlen(n1), s2 = strlen(n2), x, y, prod, carry;
+
+	idx = s1 + s2 - 1;
+	carry = 0;
+	for (x = 0; x < s1; x++)
+	{
+		for (y = 0; y < s2; y++)
+		{
+			idx = s1 + s2 - (x + y) - 1;
+			prod = (n1[s1 - x - 1] - '0') * (n2[s2 - y - 1] - '0');
+			res[idx] += (prod % 10 + carry % 10);
+			if (res[idx] > '9')
+			{
+				res[idx] -= '0';
+				carry += res[idx] - ((res[idx]) % 10);
+				res[idx] = res[idx] % 10 + '0';
+			}
+			carry = (prod + carry) / 10;
+			printf("p: %i - c: %i - n: %i\n", prod, carry, res[idx] - '0');
+		}
+	}
+	res[0] += carry;
+	if (res[0] == '0')
+		memcpy(res, res + 1, s1 + s2);
+}
+
+void multiply2(char *n1, char *n2, char *res)
+{
+	int *a, *b, *product, s1 = strlen(n1), s2 = strlen(n2), a_i, b_i, carry;
+	
+	a = malloc(sizeof(int) * (s1 + 1)), b = malloc(sizeof(int) * (s2 + 1));
+	product = calloc(s1 + s2 + 1, sizeof(int));
+
+	for (a_i = 1; a_i <= s1; a_i++)
+		a[a_i] = n1[s1 - a_i] - '0';
+	for (b_i = 1; b_i <= s2; b_i++)
+		b[b_i] = n2[s2 - b_i] - '0';
+	
+	for (b_i = 1; b_i <= s2; b_i++)
+	{
+		carry = 0;
+		for (a_i = 1; a_i <= s1; a_i++)
+		{
+			product[a_i + b_i - 1] += carry + (a[a_i] * b[b_i]);
+			carry = product[a_i + b_i - 1] / 10;
+			product[a_i + b_i - 1] %= 10;
+		}
+		product[b_i + s1] = carry;
+	}
+
+	for (a_i = 0; a_i < s1 + s2; a_i++)
+		res[a_i] = product[s1 + s2 - a_i] + '0';
+	free(product);
+	free(a);
+	free(b);
+	if (res[0] == '0')
+		memcpy(res, res + 1, s1 + s2);
+}
+
 int main(int argc, char* argv[])
 {
 	char failed = 0, *res, *n1, *n2;
-	int i, idx, s1, s2, x, y, prod, carry;
+	int i, s1, s2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -15,12 +80,13 @@ int main(int argc, char* argv[])
 	n2 = argv[2];
 	for (i = 0; n1[i] != '\0'; i++)
 		if (!isdigit(n1[i]))
+		{
 			failed = 1;
+		}
 	for (i = 0; n2[i] != '\0'; i++)
 		if (!isdigit(n2[i]))
 			failed = 1;
-	s1 = strlen(n1);
-	s2 = strlen(n2);
+	s1 = strlen(n1), s2 = strlen(n2);
 	res = malloc(s1 + s2 + 1);
 	if (res == NULL)
 		failed = 1;
@@ -36,27 +102,10 @@ int main(int argc, char* argv[])
 	for (i = 0; i < s1 + s2; i++)
 		res[i] = '0';
 	res[s1 + s2] = '\0';
-	
+
 	/* All ready */
-	idx = s1 + s2;
-	carry = 0;
-	for (x = 0; x < s1; x++)
-	{
-		for (y = 0; y < s2; y++)
-		{
-			idx = s1 - s2 - x - y - 1;
-			prod = (n1[s1 - x - 1] - '0') * (n2[s2 - y - 1] - '0');
-			res[idx] += (prod + carry) % 10; 
-			carry = (prod + carry) / 10;
-		}
-	}
-	res[0] += carry;
-	if (res[0] == '0')
-		memcpy(res, res + 1, s1 + s2);
+	multiply2(n1, n2, res);
 	printf("%s\n", res);
-	/* for (i = 0; i < s1 + s2; i++)
-		printf("%i ", res[i] - '0');
-	*/
 	free(res);
 	return (0);
 }
