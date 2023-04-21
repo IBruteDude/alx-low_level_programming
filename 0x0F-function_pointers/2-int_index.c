76152ffe93627e2cde0bb9c559b61cc4dab21ab3
IBruteDude <kamarf505@gmail.com>
2023-04-21 06:18:26 -0700

    done alhamdullilah

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,7 +8,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
+	
+	if (array == (void *)0 || cmp == (void *)0)
+		return (-1);
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			break;
