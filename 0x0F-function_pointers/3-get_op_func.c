2ca5defdcdf5cf98440bd3f4c22478e8f4234da9
IBruteDude <kamarf505@gmail.com>
2023-04-21 06:25:45 -0700

    done alhamdullilah

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,11 +18,9 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	if (s == NULL)
-		return (NULL);
 	while (i < 6 - 1)
 	{
-		if (!strcmp(ops[i].op, s))
+		if (s != NULL && !strcmp(ops[i].op, s))
 			return (ops[i].f);
 		i++;
 	}
