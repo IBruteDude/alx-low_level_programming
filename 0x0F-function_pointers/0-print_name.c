47ec6c99ced7dee069c4a88a0e72a92c5dec6dc0
IBruteDude <kamarf505@gmail.com>
2023-04-21 04:40:20 -0700

    pass name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,9 @@
-void print_name(char *name, void (*f)(char *));
+/**
+ * print_name - passes a string as an argument to a function
+ * @name: passed string
+ * @f: applied function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
