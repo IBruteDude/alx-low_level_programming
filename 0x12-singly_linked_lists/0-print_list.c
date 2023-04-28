2c4f974bda14afcfac206ee64c84c33a25210da2
IBruteDude <kamarf505@gmail.com>
2023-04-28 05:54:52 +0300

    len

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,52 +1,21 @@
 #include "lists.h"
-
 /**
- * _puts - print a simple string
- * @s: input string
- * Return: number of written chars;
+ * print_list - prints the elements of a list of nodes
+ * @h: input list head
+ * Return: number of printed nodes
  */
-int _puts(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		_putchar(s[i++]);
-
-	return (i);
-}
-
-int print_int(int n)
-{
-	while (n > 0)
-	{
-		_putchar('0' + n % 10);
-		n /= 10;
-	}
-}
-
-int format(const char *fmt, char *s, int *n)
-{
-	size_t i = 0;
-
-	while (fmt[i] != '%' && fmt[i] != '\0')
-		_putchar(fmt[i++]);
-	if (fmt[i] == '%')
-	{
-		if (fmt[++i] == 'i');
-
-	}
-}
-
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
 	while (h != NULL)
 	{
 		if (h->str != NULL)
-			_puts(h->str);
+			printf("[%d] %s", h->len, h->str);
 		else
-			
+			printf("[0] (nil)");
+		h = h->next;
+		i++;
 	}
-
+	return (i);
 }
--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -3,6 +3,48 @@
 #include <stdio.h>
 #include "lists.h"
 
+
+
+
+/**
+ * _puts - print a simple string
+ * @s: input string
+ * Return: number of written chars;
+ */
+int _puts(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		_putchar(s[i++]);
+
+	return (i);
+}
+
+void int_print(int n)
+{
+	if (n < 0)
+		_putchar('-'), n = -n;
+	while (n > 0)
+	{
+		_putchar('0' + n % 10);
+		n /= 10;
+	}
+}
+
+int format(const char *fmt, char *s, int *n)
+{
+	size_t i = 0;
+
+	while (fmt[i] != '%' && fmt[i] != '\0')
+		_putchar(fmt[i++]);
+	if (fmt[i] == '%')
+	{
+		if (fmt[++i] == 'i');
+
+	}
+}
+
 /**
  * main - check the code
  *
