ab98ebb35f490a9597cd5a36f202364d8ff7e4fa
IBruteDude <kamarf505@gmail.com>
2023-04-28 15:57:56 +0300

    Final commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,46 @@
 #include "lists.h"
-#include <stdio.h>
+
+/**
+ * print - prints a formatted string with an optional int and string
+ * @format: formatting string
+ * @n: input integer
+ * @str: input string
+ */
+void print(const char *format, int n, char *str)
+{
+	int i, mask = 1000000000U;
+
+	for (i = 0; format[i] != '\0'; i++)
+		if (format[i] == '%')
+		{
+			switch (format[++i])
+			{
+			case 'd':
+			{
+				if (n != 0)
+				{
+					while (n / mask == 0)
+						mask /= 10;
+					while (mask != 0)
+						_putchar('0' + n / mask), n %= mask, mask /= 10;
+				}
+				else
+					_putchar('0');
+				break;
+			}
+			case 's':
+			{
+				if (str)
+					while (*(str))
+						_putchar(*(str++));
+			}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
+}
 /**
  * print_list - prints the elements of a list of nodes
  * @h: input list head
@@ -12,9 +53,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
+			print("[%d] %s\n", h->len, h->str);
 		else
-			printf("[0] (nil)\n");
+			print("[%d] %s\n", 0, "(nil)");
 		h = h->next;
 		i++;
 	}
