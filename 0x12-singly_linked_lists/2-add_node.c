6520e927771087f54dfa37a470ae1ff24fdd6eb8
IBruteDude <kamarf505@gmail.com>
2023-04-28 06:00:39 +0300

    f

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,3 +1,13 @@
 #include "lists.h"
 list_t *add_node(list_t **head, const char *str)
-{}
+{
+	list_t *h;
+
+	if (head == NULL)
+		return (NULL);
+	h = *head;
+	while (h->next != NULL)
+		h = h->next;
+	h->next = malloc(sizeof(list_t));
+
+}
