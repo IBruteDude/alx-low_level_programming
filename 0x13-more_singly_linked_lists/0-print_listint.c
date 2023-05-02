5d64a34f083bed49ef8cf452f33e9387200c32d7
IBruteDude <kamarf505@gmail.com>
2023-05-02 19:05:02 +0300

    lengtit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_listint - prints the contents of a list of integers
+ * @h: input list
+ * Return: number of elements
+ */
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,2 +1,18 @@
 #include "lists.h"
 size_t listint_len(const listint_t *h);
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - prints the contents of a list of integers
+ * @h: input list
+ * Return: number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+		h = h->next, i++;
+	return (i);
+}
