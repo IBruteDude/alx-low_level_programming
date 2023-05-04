c442e161dde5470be718b63f5a79ccbce966d50e
IBruteDude <kamarf505@gmail.com>
2023-05-03 19:39:12 -0700

    FREE ALHAMDULILLAH

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,11 +3,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *h;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		h = *head;
 		*head = (*head)->next;
 		free(h);
 	}
-	head = NULL;
+	*head = NULL;
 }
