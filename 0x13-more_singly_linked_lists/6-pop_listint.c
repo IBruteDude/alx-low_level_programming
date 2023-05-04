d050cd01f55f90ad319101f379629c8b57f64f87
IBruteDude <kamarf505@gmail.com>
2023-05-04 05:46:47 +0300

    FREE ALHAMDULILLAH

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,2 +1,9 @@
 #include "lists.h"
-int pop_listint(listint_t **head);
+int pop_listint(listint_t **head)
+{
+	int i = 0;
+
+	if (head != NULL)
+		i = (*head)->n, *head = (*head)->next;
+	return (i);
+}
