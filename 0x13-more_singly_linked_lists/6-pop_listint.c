effd9ce0e9ea8ecdfb9ea7cf30ddfa681a0d5d86
IBruteDude <kamarf505@gmail.com>
2023-05-04 05:54:50 +0300

    FREE ALHAMDULILLAH

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,6 +5,6 @@ int pop_listint(listint_t **head)
 	listint_t *h;
 
 	if (head != NULL && *head != NULL)
-		h = *head, i = h->n, free(h), *head = (*head)->next;
+		h = *head, i = h->n, *head = (*head)->next, free(h);
 	return (i);
 }
