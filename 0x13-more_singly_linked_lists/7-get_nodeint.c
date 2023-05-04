4155cfba8981bcfb39e87a4423cf4a362cd34d2d
IBruteDude <kamarf505@gmail.com>
2023-05-04 05:59:51 +0300

    FREE ALHAMDULILLAH

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,2 +1,11 @@
 #include "lists.h"
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *h = head;
+
+	while (index != 0 && h != NULL)
+	{
+		h = h->next, index--;
+	}
+	return (h);
+}
