854af633b14628228143a41d22e6467477f6e6af
IBruteDude <kamarf505@gmail.com>
2023-07-08 22:52:08 +0200

    Your specs are *****, but Alhamdulillah

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *n = malloc(sizeof(hash_node_t));
 
-	if (n != NULL && ht == NULL)
+	if (n != NULL && ht != NULL)
 	{
 		n->key = strdup(key);
 		n->value = strdup(value);
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *n;
 
-	if (ht != NULL)
+	if (ht == NULL)
 		return (NULL);
 	n = ht->array[key_index((const unsigned char *)key, ht->size)];
 	while (n != NULL)
