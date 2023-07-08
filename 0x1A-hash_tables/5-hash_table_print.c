e342467687a0f82b7206f61b83c644d1715e52f9
IBruteDude <kamarf505@gmail.com>
2023-07-08 22:55:29 +0200

    Your specs are *****, but Alhamdulillah

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,29 +9,28 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	size_t first_done = 0, i;
 
-	if (ht != NULL)
+	if (ht == NULL)
+		return;
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
 	{
-		putchar('{');
-		for (i = 0; i < ht->size; i++)
+		node = ht->array[i];
+		if (node != NULL)
 		{
-			node = ht->array[i];
-			if (node != NULL)
+			while (node->next != NULL)
 			{
-				while (node->next != NULL)
-				{
-					if (first_done)
-						putchar(','), putchar(' ');
-					first_done++;
-					printf("'%s': '%s'", node->key, node->value);
-					node = node->next;
-				}
 				if (first_done)
 					putchar(','), putchar(' ');
 				first_done++;
 				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
 			}
+			if (first_done)
+				putchar(','), putchar(' ');
+			first_done++;
+			printf("'%s': '%s'", node->key, node->value);
 		}
-		putchar('}');
-		putchar('\n');
 	}
+	putchar('}');
+	putchar('\n');
 }
