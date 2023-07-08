3321529f96f1b680f2e36fc6e886a420ab2c7a88
IBruteDude <kamarf505@gmail.com>
2023-07-08 22:31:22 +0200

    Your compiler options are *****, but Alhamdulillah

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,8 +7,9 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t **nodes = ht->array, *node, *next;
+	size_t i;
 
-	for (size_t i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		node = nodes[i];
 		if (node != NULL)
