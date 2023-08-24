cd75fd9b12b6105460055f61f75f9eb254ff8078
IBruteDude <kamarf505@gmail.com>
2023-08-24 18:19:58 +0300

    Done mandetory stuff, Bismillah

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from pprint import pp
 
 
 def island_perimeter(grid):
@@ -23,4 +22,4 @@ if __name__ == "__main__":
         [0, 1, 1, 1, 0, 0],
         [0, 0, 0, 0, 0, 0],
     ]
-    pp(island_perimeter(grid))
+    print(island_perimeter(grid))
