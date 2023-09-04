b0e4ec54b2677beb91a4e104a1c152a7823a6ff7
IBruteDude <kamarf505@gmail.com>
2023-09-04 13:02:21 +0300

    added recursive stuff

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,9 +2,6 @@
 
 int skip_to(char *s1, char **s2p, char c)
 {
-	printf("s1: %s\n", s1);
-	printf("s2: %s\n", *s2p);
-	getchar();
 	if (*s1 == '\0' && **s2p != '\0')
 		return (0);
 	if (c == '\0')
@@ -38,9 +35,6 @@ int skip_to(char *s1, char **s2p, char c)
  */
 int wildcmp(char *s1, char *s2)
 {
-	printf("s1: %s\n", s1);
-	printf("s2: %s\n", s2);
-	getchar();
 	if (*s1 == *s2)
 	{
 		if (*s1 == '\0')
@@ -50,42 +44,4 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 != '*')
 		return (0);
 	return (skip_to(s1, &s2, '\0'));
-}
-#include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
-
-    r = wildcmp("main.c", "*.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*a*i*n*.*c*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "main.c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "ma********************************c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "*");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "***");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "m.*c");
-    printf("%d\n", r);
-    r = wildcmp("main.c", "**.*c");
-    printf("%d\n", r);
-    r = wildcmp("main-main.c", "ma*in.c");
-    printf("%d\n", r);
-    r = wildcmp("main", "main*d");
-    printf("%d\n", r);
-    r = wildcmp("abc", "*b");
-    printf("%d\n", r);
-    return (0);
 }
