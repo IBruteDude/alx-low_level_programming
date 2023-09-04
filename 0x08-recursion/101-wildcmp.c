152c947c2d068813266f484d95b14d5fc013b4a9
IBruteDude <kamarf505@gmail.com>
2023-09-04 12:43:19 +0300

    added recursive stuff

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,6 +2,9 @@
 
 int skip_to(char *s1, char **s2p, char c)
 {
+	printf("s1: %s\n", s1);
+	printf("s2: %s\n", *s2p);
+	getchar();
 	if (*s1 == '\0' && **s2p != '\0')
 		return (0);
 	if (c == '\0')
@@ -19,10 +22,11 @@ int skip_to(char *s1, char **s2p, char c)
 			(*s2p)++;
 			skip_to(s1, s2p, c);
 		}
+		skip_to(s1, s2p, *s1);
 	}
-	skip_to(s1, s2p, *s1);
-	if (wildcmp(s1, *s2p) == 0)
+	if (wildcmp(s1 + 1, *s2p) == 0)
 		return (skip_to(s1 + 1, s2p, '\0'));
+	return (1);
 }
 
 /**
@@ -34,6 +38,9 @@ int skip_to(char *s1, char **s2p, char c)
  */
 int wildcmp(char *s1, char *s2)
 {
+	printf("s1: %s\n", s1);
+	printf("s2: %s\n", s2);
+	getchar();
 	if (*s1 == *s2)
 	{
 		if (*s1 == '\0')
@@ -43,4 +50,42 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 != '*')
 		return (0);
 	return (skip_to(s1, &s2, '\0'));
+}
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "**.*c");
+    printf("%d\n", r);
+    r = wildcmp("main-main.c", "ma*in.c");
+    printf("%d\n", r);
+    r = wildcmp("main", "main*d");
+    printf("%d\n", r);
+    r = wildcmp("abc", "*b");
+    printf("%d\n", r);
+    return (0);
 }
