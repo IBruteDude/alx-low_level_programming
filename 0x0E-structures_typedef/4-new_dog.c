38d5d93af7b9e308f655b78542850a46250cc9c8
IBruteDude <kamarf505@gmail.com>
2023-09-05 12:29:32 +0300

    good allocating doggos, AlHamdulillah

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (d != NULL)
 	{
-		d->name = (name != NULL) ? malloc(strlen(name)) : NULL;
+		d->name = (name != NULL) ? malloc(strlen(name) + 1) : NULL;
 		if (d->name == NULL && name != NULL)
 		{
 			free(d);
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		}
 		strcpy(d->name, name);
 		d->age = age;
-		d->owner = (owner != NULL) ? malloc(strlen(owner)) : NULL;
+		d->owner = (owner != NULL) ? malloc(strlen(owner) + 1) : NULL;
 		if (d->owner == NULL && owner != NULL)
 		{
 			free(d->name);
@@ -34,3 +34,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (d);
 }
 
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code .
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *name = "Ghost";
+	char *owner = "Jon Snow";
+	dog_t *my_dog;
+
+	my_dog = new_dog(name, 4.75, owner);
+	if (my_dog == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	if (my_dog->name == name || my_dog->owner == owner)
+	{
+		printf("Duplicated strings should be stored in the allocated structure.\n");
+		return (1);
+	}
+	printf("My name is %s, I am %.2f, and my owner is %s\n",
+		my_dog->name, my_dog->age, my_dog->owner);
+	return (0);
+}
